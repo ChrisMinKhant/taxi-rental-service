1a1fb03dd15e161af64ed6307ad886a58ee8d957
Kaung Min Khant <kaungminkhant@yomabank.com>
2023-09-12 11:15:02 +0630

    initial commit

---
+++ src/main/java/com/business/taxirentalservice/TaxiRentalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.business.taxirentalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiRentalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxiRentalServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/business/taxirentalservice/TaxiRentalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business.taxirentalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiRentalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
